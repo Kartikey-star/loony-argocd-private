aada275a7e8381259fad99e9c013a93050c6b769
Kartikey Mamgain <mamgainkartikey@gmail.com>
2023-06-06 17:33:07 +0530

    update
    Signed-off-by: Kartikey Mamgain <mamgainkartikey@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-app=Flask(__name__)
+app= Flask(__name__)
 
 @app.route('/')
 def msg():
     return "Helloworld ! Deployed using argocd"
 
 if __name__ == "__main__":
-    app.Run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
