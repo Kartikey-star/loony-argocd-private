c4b2f2187990666254c94fe0912732240598bfdd
Kartikey Mamgain <mamgainkartikey@gmail.com>
2023-06-06 13:04:21 +0530

    initial commit
    Signed-off-by: Kartikey Mamgain <mamgainkartikey@gmail.com>

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def msg():
+    return "Helloworld ! Deployed using argocd"
+
+if __name__==__main__:
+    app.Run(host='0.0.0.0')
+
